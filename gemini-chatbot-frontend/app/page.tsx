701f9f4adfcc41158d40595af054d5a0a97959a1
Vraj Panchal <vraj.edu222@gmail.com>
2024-02-17 15:33:24 +0530

    fix build error

--- gemini-chatbot-frontend/app/page.tsx
+++ gemini-chatbot-frontend/app/page.tsx
@@ -1,5 +1,14 @@
+"use client";
+
 import Login from "@/components/Login";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 export default function Home() {
-  return <Login />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Login />
+    </QueryClientProvider>
+  );
 }
