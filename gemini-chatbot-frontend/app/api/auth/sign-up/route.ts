b109a409e553e3e356af6367dd557ce6f401ec38
Vraj Panchal <vraj.edu222@gmail.com>
2024-02-17 10:59:53 +0530

    fix build error

--- gemini-chatbot-frontend/app/api/auth/sign-up/route.ts
+++ gemini-chatbot-frontend/app/api/auth/sign-up/route.ts
@@ -2,7 +2,10 @@ import { UserInfoDto } from "@/api-interface/userInfoDto";
 import { signUpService } from "@/services/auth.service";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request, res: Response): Promise<UserInfoDto> {
+export async function POST(
+  req: Request,
+  res: Response
+): Promise<void | Response> {
   const dto = await req.json();
   const data = await signUpService(dto);
   return NextResponse.json(data);
