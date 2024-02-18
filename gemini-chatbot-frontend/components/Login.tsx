3d9dbfe22c6aae8120024ff299284d423bd877c9
Vraj Panchal <vraj.edu222@gmail.com>
2024-02-18 15:13:15 +0530

    added validation on login and signup page

--- gemini-chatbot-frontend/components/Login.tsx
+++ gemini-chatbot-frontend/components/Login.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { LogInDto } from "@/api-interface/logInRequestDto";
 import { useMutation } from "react-query";
 import axios from "axios";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 function Login() {
   const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
   const { mutate, isLoading } = useMutation<any, any, LogInDto>({
     mutationFn: async (logInDto: LogInDto) => {
@@ -21,10 +21,26 @@ function Login() {
     onError: ({ data }) => {},
   });
 
-  const handleLogin = () => {
+  const schema = yup.object().shape({
+    email: yup
+      .string()
+      .required("Email is required")
+      .email("Invalid email format"),
+    password: yup.string().required("Password is required"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const handleLogin = (data: LogInDto) => {
     const logInDto: LogInDto = {
-      email: email,
-      password: password,
+      email: data.email,
+      password: data.password,
     };
     mutate(logInDto);
   };
@@ -37,26 +53,36 @@ function Login() {
     <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
       <div className="bg-gray-800 p-8 rounded-lg shadow-md w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-        <form className="space-y-6">
+        <form className="space-y-6" onSubmit={handleSubmit(handleLogin)}>
           <div className="mb-4">
             <input
               type="email"
               id="email"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                errors.email ? "border-red-500" : ""
+              }`}
               placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              {...register("email")}
             />
+            {errors.email && (
+              <span className="text-red-500 mt-2">{errors.email.message}</span>
+            )}
           </div>
           <div className="mb-4">
             <input
               type="password"
               id="password"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                errors.password ? "border-red-500" : ""
+              }`}
               placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              {...register("password")}
             />
+            {errors.password && (
+              <span className="text-red-500 mt-2">
+                {errors.password.message}
+              </span>
+            )}
           </div>
           <p className="text-center">
             Don&apos;t have an account?
@@ -68,12 +94,11 @@ function Login() {
             </span>
           </p>
           <button
-            type="button"
+            type="submit"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
-            onClick={handleLogin}
             disabled={isLoading}
           >
-            {isLoading ? "Loging In..." : "Log In"}
+            {isLoading ? "Logging In..." : "Log In"}
           </button>
         </form>
       </div>
--- gemini-chatbot-frontend/components/Signup.tsx
+++ gemini-chatbot-frontend/components/Signup.tsx
@@ -2,16 +2,47 @@
 
 import { useMutation } from "react-query";
 import axios from "axios";
-import { useState } from "react";
 import { SignUpDto } from "@/api-interface/signUpRequestDto";
 import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const Signup = () => {
   const router = useRouter();
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+
+  const schema = yup.object().shape({
+    firstName: yup
+      .string()
+      .required("First name is required")
+      .max(50, "First name must be less than 50 characters"),
+    lastName: yup
+      .string()
+      .required("Last name is required")
+      .max(50, "Last name must be less than 50 characters"),
+    email: yup
+      .string()
+      .required("Email is required")
+      .email("Invalid email format"),
+    password: yup
+      .string()
+      .required("Password is required")
+      .min(4, "Password must be at least 4 characters"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    },
+    resolver: yupResolver(schema),
+  });
 
   const { mutate, isLoading } = useMutation<any, unknown, SignUpDto>({
     mutationFn: async (signUpDto: SignUpDto) => {
@@ -22,14 +53,13 @@ const Signup = () => {
     },
   });
 
-  const handleSignup = () => {
+  const handleSignup = (data: SignUpDto) => {
     const signUpDto: SignUpDto = {
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      password: password,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      email: data.email,
+      password: data.password,
     };
-
     mutate(signUpDto);
   };
 
@@ -37,52 +67,72 @@ const Signup = () => {
     <div className="flex items-center min-h-screen justify-center bg-gray-900 text-white">
       <div className="bg-gray-800 p-8 rounded-lg shadow-md w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
-        <form className="space-y-6">
+        <form className="space-y-6" onSubmit={handleSubmit(handleSignup)}>
           <div>
             <input
+              {...register("firstName")}
               type="text"
               id="firstName"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                errors.firstName ? "border-red-500" : ""
+              }`}
               placeholder="Enter your first name"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
             />
+            {errors.firstName && (
+              <span className="text-red-500 mt-2">
+                {errors.firstName.message}
+              </span>
+            )}
           </div>
           <div>
             <input
+              {...register("lastName")}
               type="text"
               id="lastName"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                errors.lastName ? "border-red-500" : ""
+              }`}
               placeholder="Enter your last name"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
             />
+            {errors.lastName && (
+              <span className="text-red-500 mt-2">
+                {errors.lastName.message}
+              </span>
+            )}
           </div>
           <div className="mb-4">
             <input
-              type="email"
+              {...register("email")}
               id="email"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                errors.email ? "border-red-500" : ""
+              }`}
               placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
             />
+            {errors.email && (
+              <span className="text-red-500 mt-2">{errors.email.message}</span>
+            )}
           </div>
           <div className="mb-4">
             <input
+              {...register("password")}
               type="password"
               id="password"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                errors.email ? "border-red-900" : ""
+              }`}
               placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
             />
+            {errors.password && (
+              <span className="text-red-500 mt-2">
+                {errors.password.message}
+              </span>
+            )}
           </div>
           <div className="mb-4">
             <button
-              type="button"
+              type="submit"
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
-              onClick={handleSignup}
               disabled={isLoading}
             >
               {isLoading ? "Signing Up..." : "Sign Up"}
