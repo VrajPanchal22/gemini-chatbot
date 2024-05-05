4510a47a2eff33dde8558028b83be3ba05847eb9
Vraj Panchal <vraj.edu222@gmail.com>
2024-05-05 16:20:18 +0530

    Fix Chat Page Design

--- gemini-chatbot-frontend/components/Chat.tsx
+++ gemini-chatbot-frontend/components/Chat.tsx
@@ -29,7 +29,7 @@ function Chat({ userData }: ChatProps) {
   const conversationId = uuidv4();
   const userId = userData.userId;
 
-  const { data } = useQuery<any>({
+  const { data, isLoading: gettingChatHistory } = useQuery<any>({
     queryKey: ["chatHistory", userId],
     queryFn: async () => {
       const response = await axios.get<any>(`api/message/get/${userId}`);
@@ -123,30 +123,40 @@ function Chat({ userData }: ChatProps) {
   };
 
   return (
-    <div className="bg-gray-900 text-white">
+    <div className="bg-gray-900 text-white min-h-screen">
       <h1 className="text-center py-2 text-xl fixed top-0 left-0 right-0 z-10 bg-gray-900">
         Hello, {userData.firstName}
       </h1>
       <div className="container mx-auto py-8">
-        <div className="lg:max-w-2xl md:max-w-xl sm:max-w-lg mx-auto mt-8 max-w-sm">
+        <div className="lg:max-w-4xl md:max-w-3xl sm:max-w-xl mx-auto mt-8">
           <div
-            className="h-screen mt-[-1rem] mb-1rem overflow-y-auto"
+            className="overflow-y-auto"
+            style={{ maxHeight: "calc(100vh - 150px)" }}
             ref={chatContainerRef}
           >
-            <div className="flex flex-col space-y-4 pb-20">
+            <div className="flex flex-col space-y-4 ">
               {/* Chat messages */}
               {chatHistory &&
                 chatHistory.map((message: Message, index: number) => (
-                  <div key={index} className="flex flex-col items-start">
-                    <span>
+                  <div
+                    key={index}
+                    className={`flex flex-col items-${
+                      message.isUser ? "start" : "end"
+                    }`}
+                  >
+                    <span className="p-2">
                       {message.isUser ? userData.firstName : "Gemini AI"}
                     </span>
-                    <div className="bg-gray-800 bg-opacity-75 rounded-xl p-6 shadow-lg shadow-gray-700/5">
+                    <div
+                      className={`rounded-xl p-6 shadow-lg shadow-gray-700/5 bg-gray-800`}
+                    >
                       {/* Display bot response content */}
                       {message.isUser ? (
                         <p className="text-white text-sm">{message.content}</p>
                       ) : (
-                        parseBotResponse(message.content)
+                        <div className="rounded-xl shadow-lg shadow-gray-700/5">
+                          {parseBotResponse(message.content)}
+                        </div>
                       )}
                     </div>
                   </div>
@@ -155,21 +165,25 @@ function Chat({ userData }: ChatProps) {
           </div>
         </div>
         {/* Input field */}
-        <div className="fixed bottom-0 left-0 right-0 p-4 shadow-md shadow-gray-700/10 mb-5">
-          <div className="flex items-center justify-between max-w-2xl mx-auto ">
-            <input
-              type="text"
-              value={newMessage}
-              placeholder="Type your message..."
-              onChange={(e) => setNewMessage(e.target.value)}
-              className="bg-gray-800 bg-opacity-75 outline-none text-white w-full p-2 ml-2 rounded-full py-2 px-4 border focus:outline-none border-white mr-3"
-            />
-            <button
-              className="bg-blue-600 hover:bg-blue-700 text-white font-bold rounded-full py-2 px-4 transition ease-in-out duration-150 outline-none"
-              onClick={handleChat}
-            >
-              Send
-            </button>
+        <div className="fixed bottom-0 left-0 right-0 p-4 shadow-md shadow-gray-700/10">
+          <div className="max-w-4xl mx-auto ">
+            <div className="flex items-center">
+              <input
+                type="text"
+                value={newMessage}
+                placeholder="Type your message..."
+                onChange={(e) => setNewMessage(e.target.value)}
+                className="bg-gray-800 bg-opacity-75 outline-none text-white flex-grow p-2 ml-2 rounded-full py-2 px-4 border focus:outline-none border-white mr-3"
+              />
+              <button
+                className="bg-blue-600 hover:bg-blue-700 text-white font-bold rounded-full py-2 px-4 transition ease-in-out duration-150 outline-none"
+                onClick={handleChat}
+              >
+                {saveUserMessageLoading || botResponseLoading
+                  ? "Loading"
+                  : "Send"}
+              </button>
+            </div>
           </div>
         </div>
       </div>
